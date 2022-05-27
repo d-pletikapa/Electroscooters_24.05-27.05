fd0a7b48b85940061cd1e76a3401fbb19cd74e3e
Dennys <36598636+keta548@users.noreply.github.com>
2022-05-27 09:16:37 +0300

    DAY3 - fix _3

--- js/index.js
+++ js/index.js
@@ -1,24 +1,24 @@
 /* const init = () => {
-    const myMap = new ymaps.Map(
-      'map',
-      {
-        center: [55.7718, 37.6316],
-        zoom: 16,
-        controls: ['smallMapDefaultSet'],
-      },
-      {},
-    );
-    const myPlacemark = new ymaps.Placemark(
-      [55.7724, 37.6252],
-      {},
-      {
-        iconLayout: 'default#image',
-        iconImageHref: 'img/mark.svg',
-        iconImageSize: [70, 70],
-        iconImageOffset: [-35, -70],
-      },
-    );
-    myMap.geoObjects.add(myPlacemark);
+	 const myMap = new ymaps.Map(
+		'map',
+		{
+		  center: [55.7718, 37.6316],
+		  zoom: 16,
+		  controls: ['smallMapDefaultSet'],
+		},
+		{},
+	 );
+	 const myPlacemark = new ymaps.Placemark(
+		[55.7724, 37.6252],
+		{},
+		{
+		  iconLayout: 'default#image',
+		  iconImageHref: 'img/mark.svg',
+		  iconImageSize: [70, 70],
+		  iconImageOffset: [-35, -70],
+		},
+	 );
+	 myMap.geoObjects.add(myPlacemark);
   };
   ymaps.ready(init);
 */
@@ -28,6 +28,6 @@ const map = L.map('map').setView([55.7726, 37.63], 17);
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png').addTo(map);
 
 L.marker([55.7724, 37.6252])
-  .addTo(map)
-  .bindPopup('E-trans')
-  .openPopup();
+	.addTo(map)
+	.bindPopup('E-trans')
+	.openPopup();
