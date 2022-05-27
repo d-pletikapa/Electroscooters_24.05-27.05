4b25ec959bc7e4fbc8f5034d877f429796159397
Dennys <36598636+keta548@users.noreply.github.com>
2022-05-27 15:25:20 +0300

    DAY-3 fix_4

--- js/index.js
+++ js/index.js
@@ -1,28 +1,38 @@
-/* const init = () => {
-	 const myMap = new ymaps.Map(
+const init = () => {
+	const myMap = new ymaps.Map(
 		'map',
 		{
-		  center: [55.7718, 37.6316],
-		  zoom: 16,
-		  controls: ['smallMapDefaultSet'],
+			center: [55.7718, 37.6316],
+			zoom: 16,
+			controls: ['smallMapDefaultSet'],
 		},
 		{},
-	 );
-	 const myPlacemark = new ymaps.Placemark(
+	);
+	const myPlacemark = new ymaps.Placemark(
 		[55.7724, 37.6252],
 		{},
 		{
-		  iconLayout: 'default#image',
-		  iconImageHref: 'img/mark.svg',
-		  iconImageSize: [70, 70],
-		  iconImageOffset: [-35, -70],
+			iconLayout: 'default#image',
+			iconImageHref: 'img/mark.svg',
+			iconImageSize: [70, 70],
+			iconImageOffset: [-35, -70],
 		},
-	 );
-	 myMap.geoObjects.add(myPlacemark);
-  };
-  ymaps.ready(init);
-*/
+	);
+	myMap.geoObjects.add(myPlacemark);
 
+	window.addEventListener('resize', () => {
+		if (window.innerWidth < 1240) {
+			myMap.setCenter([55.7724, 37.6252]);
+		} else {
+			myMap.setCenter([55.7718, 37.6316]);
+		}
+	})
+
+};
+ymaps.ready(init);
+
+
+/*
 const map = L.map('map').setView([55.7726, 37.63], 17);
 
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png').addTo(map);
@@ -31,3 +41,6 @@ L.marker([55.7724, 37.6252])
 	.addTo(map)
 	.bindPopup('E-trans')
 	.openPopup();
+*/
+
+
